801b18544c3123e070744e53bbbe1cdce18421bd
Miles Mitchell <milesm96@gmail.com>
2013-08-07 02:08:05 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title</title>
+<meta charset="UTF-8" />
+<style type="text/css">
+html
+{font-family:sans-serif;}
+</style>
+</head>
+<body>
+</body>
+</html>
