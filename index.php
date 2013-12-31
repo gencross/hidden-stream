c741269b00eba5473889addf63e01c7038ee0094
Miles Mitchell <milesm96@gmail.com>
2013-12-31 17:36:28 -0500

    Signed-off-by: Miles Mitchell <milesm96@gmail.com>

--- index.php
+++ index.php
@@ -5,11 +5,25 @@
         <title>hidden-stream</title>
     </head>
     <body>
-        <div id="container">
-            This is some text.
-        </div>
-        <?php
-        // put your code here
+         <?php
+         $rawdata = fopen("http://prod.api.pvp.net/api/lol/na/v1.2/game/by-summoner/25714810/recent?api_key=0e93152b-898d-41a7-8b74-39a609fcec81", "r");
+         $data = json_decode($rawdata, TRUE);
+         $gold = $data[games][0][statistics][2][value];
+       // $ch = curl_init();
+       // curl_setopt($ch, CURLOPT_URL, "http://www.example.com/");
+       // curl_setopt($ch, CURLOPT_HEADER, false);
         ?>
+        <div id="container">
+            <table>
+                <tr>
+                    <th>Stat</th>
+                    <th>Value</th>
+                </tr>
+                <tr>
+                    <td>Gold Earned</td>
+                    <td><?php echo $gold ?></td>
+                </tr>
+            </table>
+        </div>
     </body>
 </html>
