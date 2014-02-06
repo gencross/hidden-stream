5256c0dbd8883d138566aedef09d6f9d469ba3c4
Miles <milesm96@gmail.com>
2014-02-06 11:29:51 -0800

    example

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
 <!DOCTYPE html>
-<html>
+<html id="all">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>hidden-stream</title>
     </head>
     <body>
+    	<?php
+    		if ($_POST["number"]) {
+    			$number = "Your number was " . $_POST["number"] . ".";
+    			}
+    		else {
+    			$number = "You didn't pick a number!";
+    			}
+    	?>
     	<h1>A new side project</h1><br />
+    	
+    <!--	
     	<a href="league.php">Some league thing</a><br />
     	<a href="http://damp-cove.herokuapp.com">Some other thing that doesn't work</a><br />
     	<a href="git://git@heroku.com:hidden-stream.git">Git</a>
+    -->
+    
+    	<p>
+    		<?php
+    			echo $number;
+    		?>
+    	</p>
+    	
+    	<form id="myForm" action="index.php" method="POST">
+    		<input type="text" name="number"></input>
+    		<input type="sumbit" />
+    	</form>
+    	
     </body>
 </html>
